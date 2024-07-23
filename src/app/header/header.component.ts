1a4818e56074f98af6fde035337f5828af8a6b8b
02Veljko02 <veljkojovkovic1999@gmail.com>
2024-07-23 15:42:32 +0200

    Initial commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+// src/app/header/header.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  userName: string = 'Veljko Jovkovic';
+  messageCount: number = 12;
+}
