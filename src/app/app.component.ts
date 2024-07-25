559e08b4e4ddea04c1d588d4bcbe11866bf02690
02Veljko02 <veljkojovkovic1999@gmail.com>
2024-07-25 17:17:41 +0200

    5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { TextboxeslistComponent } from './textboxeslist/textboxeslist.component';
+import { TransactionsListComponent } from './transactions-list/transactions-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, HeaderComponent, TextboxeslistComponent],
+  imports: [RouterOutlet, MenuComponent, HeaderComponent, TextboxeslistComponent, TransactionsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
