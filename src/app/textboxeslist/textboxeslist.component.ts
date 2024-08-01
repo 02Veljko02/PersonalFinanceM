01d0b3bc4ca5c5681858e951e84aebf57a697171
02Veljko02 <veljkojovkovic1999@gmail.com>
2024-08-01 22:04:43 +0200

    29

--- src/app/textboxeslist/textboxeslist.component.ts
+++ src/app/textboxeslist/textboxeslist.component.ts
@@ -7,22 +7,4 @@ import { Component } from '@angular/core';
   templateUrl: './textboxeslist.component.html',
   styleUrls: ['./textboxeslist.component.css']
 })
-export class TextboxeslistComponent {
-  selectedOption = 'Select an option';
-  isDropdownOpen = false;
-
-  toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
-  }
-
-  selectOption(event: Event, option: string) {
-    event.preventDefault(); // Sprečava podrazumevano ponašanje linka
-    this.selectedOption = option;
-    this.isDropdownOpen = false;
-  }
-
-  clearDropdown() {
-    this.selectedOption = 'Select an option';
-    this.isDropdownOpen = false;
-  }
-}
+export class TextboxeslistComponent {}
